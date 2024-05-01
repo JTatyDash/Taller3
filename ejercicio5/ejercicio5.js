8faf47356e5578a89f42069678832b741dd7fed2
JTatyDash <jenny021920@gmail.com>
2024-05-01 18:50:33 -0500

    Ejercicio5 Resuelto

--- ejercicio5/ejercicio5.js
+++ ejercicio5/ejercicio5.js
@@ -1,2 +1,10 @@
 /*Escribe una función para calcular el área de un círculo. Puedes
 utilizar funciones declarativas.*/
+
+function areaCircle(radio) {
+    const area = Math.PI * radio * radio;
+    return area;
+}
+const radio = 5;
+const area = areaCircle(radio);
+console.log(`El área del círculo con radio ${radio} es: ${area}`);
