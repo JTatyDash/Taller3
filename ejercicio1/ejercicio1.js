aaecf093a2cff08bae702c2caa4554ab4b410d97
JTatyDash <jenny021920@gmail.com>
2024-05-01 17:22:14 -0500

    Ejercicio1 Resuleto

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -1,8 +1,12 @@
 /* Escribe una función que tome dos números como parámetros y
 devuelva su suma */
 
+let number1 = 0;
+let number2 = 0;
+
 function sum(number1, number2) {
-    suma = number1 + number2;
+    const suma = number1 + number2;
+    console.log("La suma es: "+ suma);
 }
 
-sum();
+sum(2, 5);
