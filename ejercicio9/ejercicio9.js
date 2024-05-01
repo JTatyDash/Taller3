ab9bdcab0942d6614b80920ef72833210d7f1d20
JTatyDash <jenny021920@gmail.com>
2024-05-01 18:21:08 -0500

    Ejercicio9 Resuelto

--- ejercicio9/ejercicio9.js
+++ ejercicio9/ejercicio9.js
@@ -1,2 +1,13 @@
 /*Escribe una función que genere la tabla de multiplicar de un número
 específico.*/
+
+function tablaMultiplicar(number){
+    console.log(`Tabla de multiplicar del ${number}`);
+    const result = [];
+    for (let i = 1; i <= 10; i++){
+        result.push(`${number} x ${i} = ${number * i}`);
+    }
+    result.forEach(result => console.log(result));
+}
+
+tablaMultiplicar(8);
