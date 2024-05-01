1809330847b651e1415aca8d5c6467dcccef14d1
JTatyDash <jenny021920@gmail.com>
2024-05-01 18:41:11 -0500

    Ejercicio3 Resuelto

--- ejercicio3/ejercicio3.js
+++ ejercicio3/ejercicio3.js
@@ -1,2 +1,11 @@
 /*Crea una función que ordene un array de números de manera
 ascendente. Puedes utilizar funciones flecha.*/
+
+function orderNumbers(numbers) {
+    console.log("Lista de numeros desorganizada: ", disorganized);
+    const organized = numbers.sort((a, b) => a - b);
+    return organized;
+}
+    disorganized = [5, 2, 4, 1, 3, 6, 8, 10, 7, 9];
+    organized = orderNumbers(disorganized);
+    console.log("Lista de numeros organizada: ", organized);
