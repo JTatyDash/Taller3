5e33fad3f79d06fe786d7ad400d2328e280abfae
JTatyDash <jenny021920@gmail.com>
2024-05-01 19:03:17 -0500

    Ejercicio7 Resuelto

--- ejercicio7/ejercicio7.js
+++ ejercicio7/ejercicio7.js
@@ -1,2 +1,25 @@
 /*Implementa una función que verifique si una palabra es un palíndromo (se
 lee igual de izquierda a derecha que de derecha a izquierda).*/
+
+function esPalindromo(word) {
+    // Convertir la palabra a minúsculas y eliminar espacios en blanco
+    word = word.toLowerCase().replace(/ /g, "");
+
+    // Obtener la longitud de la palabra
+    const longitud = word.length;
+
+    // Comparar caracteres desde ambos extremos de la palabra
+    for (let i = 0; i < longitud / 2; i++) {
+        if (word[i] !== word[longitud - 1 - i]) {
+        return false; // No es palíndromo
+    }
+    }
+
+    // Si el bucle no encuentra diferencias, la palabra es palíndromo
+    return true;
+}
+    const words = ["radar", "amor", "orejera", "revolver", "ana"];
+    for (const word of words) {
+    const resultado = esPalindromo(word) ? "sí" : "no";
+    console.log(`${word} es palíndromo: ${resultado}`);
+}
